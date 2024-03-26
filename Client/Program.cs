99ea18bde8b50b51d33f0cb29a3f954df2675795
rootguillen <guillenaroot@gmail.com>
2024-03-26 20:31:00 +0800

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,16 @@
+using Blazored.Toast;
+using IPOPHLTrademark.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSyncfusionBlazor();
+builder.Services.AddBlazorBootstrap();
+builder.Services.AddBlazoredToast();
+
+await builder.Build().RunAsync();
---
+++ Server/Controllers/FileActionController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Hosting;
+
+
+namespace BlazorFileUploadJWT.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileActionController : Controller
+    {
+        private Microsoft.AspNetCore.Hosting.IHostingEnvironment hostingEnv;
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public FileActionController(Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
+        {
+            this.hostingEnv = env;
+        }
+
+        [HttpPost("[action]")]
+        public async void Save(IList<IFormFile> UploadFiles)
+        {
+            //to get authorization Header to handle save file on server side  
+            var authorizationHeader = Request.Headers["Authorization"];
+            if (authorizationHeader.Count == 0 || authorizationHeader[0] != "test123")
+            {
+                Response.Clear();
+                Response.StatusCode = 401;
+                Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = "Unauthorized";
+                return;
+            }
+            try
+            {
+                foreach (var file in UploadFiles)
+                {
+                    if (UploadFiles != null)
+                    {
+                        var filename = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                        filename = hostingEnv.WebRootPath + $@"\{filename}";
+                        if (!System.IO.File.Exists(filename))
+                        {
+                            using (FileStream fs = System.IO.File.Create(filename))
+                            {
+                                file.CopyTo(fs);
+                                fs.Flush();
+                            }
+                        }
+                        else
+                        {
+                            Response.Clear();
+                            Response.StatusCode = 204;
+                            Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = "File already exists.";
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Response.Clear();
+                Response.ContentType = "application/json; charset=utf-8";
+                Response.StatusCode = 204;
+                Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = "File failed to upload";
+                Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = e.Message;
+            }
+
+        }
+        [HttpPost("[action]")]
+        public void Remove(IList<IFormFile> UploadFiles)
+        {
+            //to get authorization Header to handle save file on server side  
+            var authorizationHeader = Request.Headers["Authorization"];
+            if (authorizationHeader.Count == 0 || authorizationHeader[0] != "test123")
+            {
+                Response.Clear();
+                Response.StatusCode = 401;
+                Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = "Unauthorized";
+                return;
+            }
+            try
+            {
+                var filename = hostingEnv.ContentRootPath + $@"\{UploadFiles[0].FileName}";
+                if (System.IO.File.Exists(filename))
+                {
+                    System.IO.File.Delete(filename);
+                }
+            }
+            catch (Exception e)
+            {
+                Response.Clear();
+                Response.StatusCode = 200;
+                Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = "File removed successfully";
+                Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = e.Message;
+            }
+        }
+    }
+}
